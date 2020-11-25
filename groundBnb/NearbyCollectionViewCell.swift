771f851d53a06cdd95ff2b1c62d45d85ce124001
Shalon Teoh <shalon@exabytes.com>
2020-11-25 19:03:46 +0800

    feat(nearby): Add nearby place list in home screen

---
+++ groundBnb/NearbyCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  NearbyCollectionViewCell.swift
+//  groundBnb
+//
+//  Created by Shalon Teoh on 23/11/2020.
+//
+
+import UIKit
+
+class NearbyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var placeLabel: UILabel!
+    @IBOutlet var distanceLabel: UILabel!
+}
--- groundBnb/ViewController.swift
+++ groundBnb/ViewController.swift
@@ -7,12 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    let nearbyList : [ [String : String] ] = [
+        ["place":"Tanjung Tokong", "distance":"20KM away"],
+        ["place":"Bayan Lepas", "distance":"5KM away"],
+        ["place":"Jelutong", "distance":"10KM away"],
+        ["place":"Georgetown", "distance":"15KM away"],
+        ["place":"Butterworth", "distance":"40KM away"],
+        ["place":"Air Itam", "distance":"15KM away"],
+    ]
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return nearbyList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "nearbyCell", for: indexPath) as! NearbyCollectionViewCell
+        
+        cell.imageView.image = UIImage(named: "nearby\(indexPath.row+1)")
+        cell.placeLabel.text = nearbyList[indexPath.row]["place"]
+        cell.distanceLabel.text = nearbyList[indexPath.row]["distance"]
+        return cell
+    }
+      
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
